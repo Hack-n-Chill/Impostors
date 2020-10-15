845cfa5835e1314fd7596ca786b45542db684c37
Amit Chakraborti <amit.chakraborti09@gmail.com>
2020-10-15 18:37:17 +0530

    added google login

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const bodyParser=require('body-parser');
 const express=require('express');
 const app=express();
 const mongoose=require('mongoose');
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
+require('dotenv').config();
 
 
 app.set('view engine','ejs');
@@ -13,7 +19,31 @@ app.use(express.static(path.join(__dirname,'public')));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+  }));
+  
+  app.use(passport.initialize());
+  app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/coviddb",{useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false});
+mongoose.set("useCreateIndex", true);
+
+const userSchema = new mongoose.Schema ({
+    email: String,
+    password: String,
+    googleId: String,
+    secret: String
+  });
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
 
 const hospitalSchema = new mongoose.Schema({
     name: String,
@@ -26,12 +56,39 @@ const hospitalSchema = new mongoose.Schema({
 
 const hospital=mongoose.model("Hospital",hospitalSchema);
 
+
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/success",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
 app.get('/',(req,res)=>{
     res.render('index');
 });
 
+app.get("/auth/google",
+  passport.authenticate('google', { scope: ["profile"] })
+);
+
 app.get('/admin/hospital',(req,res)=>{
     res.render('adminpage');
+});
+
+app.get("/login", function(req, res){
+  res.render("login");
+});
+
+app.get("/register", function(req, res){
+  res.render("register");
 });
 
 app.get('/new-user/patient',(req,res)=>{
--- public/js/admin.js
+++ public/js/admin.js
@@ -11,4 +11,7 @@ function showPosition(position) {
     document.getElementsByName("lati").value = position.coords.latitude; 
   alert( document.getElementsByName("lati").value);
   document.getElementsByName("longi").value = position.coords.longitude;
+  // $( "input[name='lati']" ).val( position.coords.latitude);
+  // $("input[name='longi']").val(position.coords.longitude);
+  
 }
